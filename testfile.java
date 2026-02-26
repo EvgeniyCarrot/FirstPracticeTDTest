62ad6ee15eda8ec72cfa0dc8088377d691bbd41b
Марковкин Евгений <evgenymarkovkin@MacBook-Pro-Markovkin.local>
2026-02-26 12:00:00 +0300

    Create second file

--- testfile.java
+++ testfile.java
@@ -1,18 +1,14 @@
 public class testfile {
     public static void main(String[] args) {
+        whileEx();
+    }
+
+    public static boolean whileEx(){
+        int value = 10;
+        boolean result = true;
+        while (value > 0){
+            result = false;
+        }
+        return result;
     }
 }
-
-
-
-// whileEx();
-//     }
-
-//     public static boolean whileEx(){
-//         int value = 10;
-//         boolean result = true;
-//         while (value > 0){
-//             result = false;
-//         }
-//         return result;
-//     }
